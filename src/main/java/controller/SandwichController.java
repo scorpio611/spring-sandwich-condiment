3adcf7274211198e944697056622efc4864a9aa4
scorpio611 <evilmonter666@gmail.com>
2019-01-15 14:21:40 +0700

    [Bài tập] Ứng dụng hiển thị gia vị với Sandwich

--- src/main/java/controller/SandwichController.java
+++ src/main/java/controller/SandwichController.java
@@ -5,12 +5,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SandwichController {
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public ModelAndView home() {
+        return new ModelAndView("home");
     }
 
     @PostMapping("/save")
